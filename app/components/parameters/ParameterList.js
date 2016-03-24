9904ce2f4848dd85cf5fcee8ec0e03c924fac677
Tobias Bales <tobias.bales@gmail.com>
2016-03-24 20:42:33 +0100

    Responsive cards

--- app/components/parameters/ParameterList.js
+++ app/components/parameters/ParameterList.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { Card, CardTitle, CardText } from 'react-toolbox/lib/card';
-import Column from '../Column';
 
 export default class ParamterList extends Component {
   static propTypes = {
@@ -11,14 +10,14 @@ export default class ParamterList extends Component {
   render() {
     const { children, name } = this.props;
     return (
-      <Column width={50} style={{ padding: '5px' }}>
-        <Card className="parameter-list">
+      <div className="parameter-list">
+        <Card>
           <CardTitle title={name} />
           <CardText>
             {children}
           </CardText>
         </Card>
-      </Column>
+      </div>
      );
   }
 }
