9a928c23843761b8d4c4fcdf2bbb1dcb20fdc1cd
Tobias Bales <tobias.bales@gmail.com>
2016-04-25 08:08:31 +0200

    fix(pixler): make output selectable

--- app/pixler/Output.js
+++ app/pixler/Output.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Card, CardText, CardTitle } from 'react-toolbox/lib/card';
+import styles from './Output.css';
 
 export default class Output extends Component {
   static propTypes = {
@@ -8,15 +9,17 @@ export default class Output extends Component {
   }
 
   _renderShape() {
-    return this.props.shape.map((byte, i) => (
-      <span key={i}>0x{byte.toString(16)}, </span>
-    ));
+    return this.props.shape.map((byte, i) => {
+      const text = `0x${byte.toString(16)}, `;
+      return <span key={i} className={styles.data}>{text}</span>;
+    });
   }
 
   _renderOutline() {
-    return this.props.outline.map((byte, i) => (
-      <span key={i}>0x{byte.toString(16)}, </span>
-    ));
+    return this.props.outline.map((byte, i) => {
+      const text = `0x${byte.toString(16)}, `;
+      return <span key={i} className={styles.data}>{text}</span>;
+    });
   }
 
   render() {
@@ -25,11 +28,11 @@ export default class Output extends Component {
         <CardTitle title="output" subtitle="this can be copied to the font file" />
         <CardText>
           <div>Shape</div>
-          <div>{this._renderShape()}</div>
+          <div className={styles.data}>{this._renderShape()}</div>
         </CardText>
         <CardText>
           <div>Outline</div>
-          <div>{this._renderOutline()}</div>
+          <div className={styles.data}>{this._renderOutline()}</div>
         </CardText>
       </Card>
     );
