e494b3c194f9b7ec269266b9428aa82e7c319431
Tobias Bales <tobias.bales@gmail.com>
2016-05-12 19:48:44 +0200

    refactor(preview): use same logic as osd for home direction drawing

--- app/config/preview/HomeDirection.js
+++ app/config/preview/HomeDirection.js
@@ -22,21 +22,26 @@ export default class Wind extends PreviewBase {
       this.canvas.save();
       this.canvas.translate(height / 2, height / 2);
       this.canvas.rotate(bearing * Math.PI / 180);
-      this.canvas.translate(- height / 2, - height / 2);
-      this.canvas.drawFilledRectangle(7, 8, 5, 10);
-      this.canvas.drawLine(4, 8, 15, 8);
-      this.canvas.drawLine(5, 7, 14, 7);
-      this.canvas.drawLine(6, 6, 13, 6);
-      this.canvas.drawLine(7, 5, 12, 5);
-      this.canvas.drawLine(8, 4, 11, 4);
-      this.canvas.drawLine(9, 3, 10, 3);
-      this.canvas.drawLine(4, 8, 10, 2, false, true);
-      this.canvas.drawLine(10, 2, 15, 8, false, true);
-      this.canvas.drawLine(15, 8, 12, 8, false, true);
-      this.canvas.drawLine(4, 8, 7, 8, false, true);
-      this.canvas.drawLine(12, 8, 12, 18, false, true);
-      this.canvas.drawLine(7, 8, 7, 18, false, true);
-      this.canvas.drawLine(7, 18, 12, 18, false, true);
+
+      this.canvas.drawLine(-2, -2, -2, 8);
+      this.canvas.drawLine(-1, -2, -1, 8);
+      this.canvas.drawLine(0, -2, 0, 8);
+      this.canvas.drawLine(1, -2, 1, 8);
+      this.canvas.drawLine(2, -2, 2, 8);
+      this.canvas.drawLine(-5, -3, 5, -3);
+      this.canvas.drawLine(-4, -4, 4, -4);
+      this.canvas.drawLine(-3, -5, 3, -5);
+      this.canvas.drawLine(-2, -6, 2, -6);
+      this.canvas.drawLine(-1, -7, 1, -7);
+      this.canvas.drawLine(0, -8, 0, -8);
+
+      this.canvas.drawLine(-7, -2, 0, -9, false, true);
+      this.canvas.drawLine(0, -9, 7, -2, false, true);
+      this.canvas.drawLine(7, -2, 3, -2, false, true);
+      this.canvas.drawLine(-7, -2, -3, -2, false, true);
+      this.canvas.drawLine(3, -2, 3, 9, false, true);
+      this.canvas.drawLine(-3, -2, -3, 9, false, true);
+      this.canvas.drawLine(-3, 9, 3, 9, false, true);
       this.canvas.restore();
     }
   }
