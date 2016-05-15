1157c798073d919b592a7c8b45c497c29d9c40ad
Tobias Bales <tobias.bales@gmail.com>
2016-05-15 22:15:38 +0200

    style(components): match Text styling to other components

--- app/config/parameters/Text.js
+++ app/config/parameters/Text.js
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react';
 import Label from '../../components/Label';
+import styles from './Text.css';
 
 export default function Text(props) {
   const { label, text } = props;
   return (
-    <div>
+    <div className={styles.container}>
       <Label text={label} />
       <div>{text}</div>
     </div>
